79c239384311ca56635223e9805eb870a8f257c8
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-02-06 08:38:59 -0300

    refactor: rename routes variable and update export structure

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import StudentRouter from "./students.routes";
 import userRouter from "./users.routes"
 
-const routes = Router();
+const router = Router();
 
 // Rota raiz para teste
-routes.get("/", (req, res) => {
+router.get("/", (req, res) => {
   return res.status(200).json({ message: "Vai Tomando!" });
 });
 
 router.use("/students", StudentRouter);
-router.use("/users", userRouter)
+router.use("/users", userRouter);
 
-export default routes;
+export { router };
