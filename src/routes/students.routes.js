27689ed87c257b6ad5605dd7fba4298b6c6abd66
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-02-06 08:22:55 -0300

    feat: add routes for student management

---
+++ src/routes/students.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import {
+    getStudents,
+    getStudentById,
+    createStudent,
+    updateStudent,
+    deleteStudent
+} from '../controllers/Student.controller';
+
+const StudentRouter = Router();
+
+StudentRouter.get('/', getStudents
+);
+StudentRouter.get('/:id', getStudentById);
+StudentRouter.get('/', createStudent);
+StudentRouter.get('/:id', updateStudent);
+StudentRouter.get('/:id', deleteStudent);
+
+export default StudentRouter;
